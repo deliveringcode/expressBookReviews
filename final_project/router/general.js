98c5c761b5930b9a2bdad7b05d41465f5fe7f712
Freddy <fasdtheunique_26@hotmail.com>
2026-06-15 17:42:45 -0400

    route byauthor

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -19,13 +19,19 @@ public_users.get('/',function (req, res) {
 public_users.get('/isbn/:isbn',function (req, res) {
   const isbn = req.params.isbn;
   res.send(books[isbn]);
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const keys = Object.values(books);
+  const data = keys.filter(book => book.author === author);
+  if(data.length > 0){
+    return res.status(200).json(data);
+  }
+  else
+    return res.status(404).json({message: "Author not found"});
+  
 });
 
 // Get all books based on title
